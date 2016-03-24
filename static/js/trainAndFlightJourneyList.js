22433632d63f6d4545c7ad32c706a676c6b81179
shekhar1989 <shekhar.gupta@hotmail.com>
2016-03-24 13:18:15 +0530

    gui updates

--- static/js/trainAndFlightJourneyList.js
+++ static/js/trainAndFlightJourneyList.js
@@ -8,8 +8,9 @@ function showtransportJourneyList(transportList, mode){
 		var journeyDividerContent = "";
 		for (j = 0; j < transportList[i][0].parts.length;j++ ){
 			var transportDetails = transportList[i][0].parts[j];
-			if(j!=0){
-				details = details + "&nbsp;<hr>&nbsp;";
+			if(j==0){
+				//source and details of begining station
+				details = details + "<table width=\"100%\" style=\"color:grey\"><tr><td><table width=\"100%\"><tr><td width=\"20%\" class = \"detailsCity\">"+transportDetails.source+"&nbsp;&nbsp;</td><td class = \"detailsTime\">  Dep : "+transportDetails.departure+"</td></tr></table></td></tr>";
 			}
 			if(first == 1 && transportDetails.mode == mode){
 				if(mode == "flight"){
@@ -30,16 +31,32 @@ function showtransportJourneyList(transportList, mode){
 			if(transportDetails.mode != "train"){
 				siteName = transportDetails.site;
 			}
-			var travelSpecificWid = travelSpecificsWidget(transportDetails.source,transportDetails.destination,transportDetails.arrival,transportDetails.departure,transportDetails.duration);
-			details = details + "<div class=\"row-eq-height\"><div class=\"col-sm-3 col-height col-middle\" style =\"text-align:left\"><font color = \"grey\"><b>"+transportDetails.carrierName+"</b><br/>&nbsp;</div><div class=\"col-sm-6 col-height col-middle\" style =\"text-align:center\">"+travelSpecificWid+"</div></font><div class=\"col-sm-3 col-height col-middle\"><table width = \"100%\" style =\"text-align:right\"><tr><td>"+siteName+"&nbsp;<button type=\"button\" class=\"btn btn-success btn-arrow-right\">"+transportDetails.price+"/-</button></td></tr></table></div></div>";
 			
+			if(transportDetails.mode == "flight"){
+					transportCarrier = "<img src=\"/images/"+transportDetails.carrierName+".png\"></img><br/>";
+				} else {
+					transportCarrier = transportDetails.carrierName;
+				}
+			//details of the transportation mode
+			details = details + "<tr><td><table width=\"100%\"><tr><td width=\"5%\"><table><tr><td style=\"white-space: nowrap;\"><img src=\"/images/"+transportDetails.mode+"2.png\">&nbsp;&nbsp;</td><td><div style=\"border-left:1px solid #808080;border-left-style:dotted;height:150px\"></div></td><td style=\"white-space: nowrap;text-align:left;\">&nbsp;&nbsp;<b class=\"detailsMode\">"+transportCarrier+"</b><br/><div class=\"detailsPrice\">&nbsp;&nbsp;&nbsp;<b class=\"detailsLabel\">Price : </b>Rs "+transportDetails.price+"/-</div><div class=\"detailsDuration\">&nbsp;&nbsp;&nbsp;<b class=\"detailsLabel\">Duration : </b>"+transportDetails.duration+" hrs</div></td></tr></table></td><td width=\"95%\" style=\"text-align:right\">"+siteName+"&nbsp;&nbsp;<button type=\"button\" class=\"btn btn-success btn-arrow-right\" id = \"hello\">Book</button>&nbsp;&nbsp;&nbsp;</td></tr></table></td></tr>";
+			
+			if(j==transportList[i][0].parts.length-1){
+				//details of last station
+				details = details + "<tr><td><table width=\"100%\"><tr><td width=\"20%\" class = \"detailsCity\">"+transportDetails.destination+"&nbsp;&nbsp;</td><td class = \"detailsTime\">  Arr : "+transportDetails.arrival+"</td></tr></table></td></tr></table>";
+			} else {
+				//datails of intermediate station
+				k=j+1;
+				details = details + "<tr><td><table width=\"100%\"><tr><td width=\"20%\" class = \"detailsCity\">"+transportDetails.destination+"&nbsp;&nbsp;</td><td><table><tr><td class = \"detailsTime\">  Arr : "+transportDetails.arrival+"</td></tr><tr><td class = \"detailsTime\">  Dep : "+transportList[i][0].parts[j].departure+"</td></tr></table></td></tr></table></td></tr>"
+			}
+			
+			//bar displaying journey division on the main widget
 			var journeyDividerContent = journeyDividerContent + "<td style =\"padding: 1px\"><img src=\"/images/"+transportDetails.mode+"2.png\"/></td>";
 		}
 			var travelSpecificWid = travelSpecificsWidget(transportTotalDetails.source,transportTotalDetails.destination,transportTotalDetails.arrival,transportTotalDetails.departure,transportTotalDetails.duration);
 			
 			var journeyDivider = "<br/><table class=\"table table-bordered\" style =\"text-align:center\"><tbody><tr>"+journeyDividerContent+"</tr></tbody></table>"
 
-				output = output + "<div class=\""+mode+"Details\" id = \"details"+transportTotalDetails.id+"\"><div class=\"panel panel-default\"><div class=\"panel-heading\"><table width=\"100%\"><tr><td style =\"text-align:left\"><h4><font color=\"CornflowerBlue\">&nbsp;&nbsp;"+transportTotalDetails.duration+" Hr</font></h4></td><td style =\"text-align:right\"><h4><font color=\"green\">Rs "+transportTotalDetails.price+"/-&nbsp;</font></h4></td></tr></table></div><div class=\"panel-body\">"+details+"</div></div></div>";
+				
 				var numberOfChanges = transportList[i][0].parts.length-1
 				if (numberOfChanges == 1){
 					var numberOfChangesView = numberOfChanges + " Stop"
@@ -48,42 +65,17 @@ function showtransportJourneyList(transportList, mode){
 				} else {
 					var numberOfChangesView = numberOfChanges + " Stops"
 				}
-				output = output + "<div class=\""+mode+"Main\" id = \"main"+transportTotalDetails.id+"\"><div class=\"panel panel-default\"><div class=\"panel-body\"><div class=\"row-eq-height\"><table width = \"100%\"><tr><td style =\"text-align:left\"><font color = \"grey\"><b>"+transportTotaljourney+"</b></font></td><td width = \"25%\" style =\"text-align:right\"><button type=\"button\" class=\"btn btn-warning\" id = \""+transportTotalDetails.id+"\">Select</button></td></tr></table></div><div class=\"row-eq-height\">"+journeyDivider+"</div><div class=\"row-eq-height\"><div class=\"col-sm-3 col-height col-middle\" style =\"text-align:left\"><font color = \"grey\">"+numberOfChangesView+"<br/>&nbsp;</div><div class=\"col-sm-6 col-height col-middle\" style =\"text-align:center\">"+travelSpecificWid+"</div></font><div class=\"col-sm-3 col-height col-middle\"><table width = \"100%\" style =\"text-align:right\"><tr><td><h4><font color=\"green\">"+transportTotalDetails.price+"/-</font><h4></td></tr></table></div></div></div></div></div>";
-
+				output = output + "<div class=\""+mode+"Main\" id = \"main"+transportTotalDetails.id+"\"><div class=\"panel panel-default\"><div class=\"panel-body\"><div class=\"row-eq-height\"><table width = \"100%\"><tr><td style =\"text-align:left\"><font color = \"grey\"><b>"+transportTotaljourney+"</b></font></td><td width = \"25%\" style =\"text-align:right\"><button type=\"button\" class=\"btn btn-warning\"  data-toggle=\"modal\" data-target=\"#details"+transportTotalDetails.id+"\">Select</button></td></tr></table></div><div class=\"row-eq-height\">"+journeyDivider+"</div><div class=\"row-eq-height\"><div class=\"col-sm-3 col-height col-middle\" style =\"text-align:left\"><font color = \"grey\">"+numberOfChangesView+"<br/>&nbsp;</div><div class=\"col-sm-6 col-height col-middle\" style =\"text-align:center\">"+travelSpecificWid+"</div></font><div class=\"col-sm-3 col-height col-middle\"><table width = \"100%\" style =\"text-align:right\"><tr><td><h4><font color=\"green\">"+transportTotalDetails.price+"/-</font><h4></td></tr></table></div></div></div></div></div>";
+				
+				output = output + "<div class=\"modal fade\" id=\"details"+transportTotalDetails.id+"\" role=\"dialog\"><div class=\"modal-dialog\"><div class=\"modal-content\"><div class=\"modal-header\"><button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button></div><div class=\"modal-body\">"+details+"</div><div class=\"modal-footer\"><button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button></div></div></div></div>";
+				
 	}
 	//binding data to flights tab
 	if(mode == "flight"){
 		document.getElementById("flightData").innerHTML = output;	
-		for (i = 0; i < transportList.length; i++){
-		
-		var transportMainId = transportList[i][0].full[0].id;
-		$( "#"+transportMainId ).click(function() {
-				var thisId = this.id;
-				var mainId = "main"+thisId;
-				var detailsId = "details"+thisId;
-				$(".flightDetails").hide();
-				$(".flightMain").show();
-				$("#"+mainId).hide();
-				$("#"+detailsId).show();
-		});
-		}	
 	} else { //binding data to trains tab
 		document.getElementById("trainData").innerHTML = output;
-		for (i = 0; i < transportList.length; i++){
-		
-		var transportMainId = transportList[i][0].full[0].id;
-		$( "#"+transportMainId ).click(function() {
-				var thisId = this.id;
-				var mainId = "main"+thisId;
-				var detailsId = "details"+thisId;
-				$(".trainDetails").hide();
-				$(".trainMain").show();
-				$("#"+mainId).hide();
-				$("#"+detailsId).show();
-		});
-		}	
 	}
-	$("."+mode+"Details").hide();
 	
 	
 }
